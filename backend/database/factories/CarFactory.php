b81c3f4df1721c6d796637023118b11b2de77586
abdelhamid <ourighli433@gmail.com>
2026-09-19 20:36:16 +0100

    fix: realistic prices and energy types per car model in seeders

--- backend/database/factories/CarFactory.php
+++ backend/database/factories/CarFactory.php
@@ -13,25 +13,25 @@ class CarFactory extends Factory
 
     public function definition(): array
     {
-        // [brand, model, type, seats]
+        // [brand, model, type, seats, min price, max price (MAD / day), possible energies]
         $models = [
-            ['Dacia', 'Logan', 'sedan', 5],
-            ['Dacia', 'Sandero', 'hatchback', 5],
-            ['Dacia', 'Duster', 'suv', 5],
-            ['Renault', 'Clio', 'hatchback', 5],
-            ['Peugeot', '208', 'hatchback', 5],
-            ['Peugeot', '3008', 'suv', 5],
-            ['Volkswagen', 'Golf', 'hatchback', 5],
-            ['Toyota', 'Corolla', 'sedan', 5],
-            ['Hyundai', 'Tucson', 'suv', 5],
-            ['Kia', 'Picanto', 'hatchback', 4],
-            ['Mercedes', 'Classe C', 'sedan', 5],
-            ['BMW', 'Série 4', 'coupe', 4],
-            ['Ford', 'Transit', 'van', 9],
+            ['Dacia', 'Logan', 'sedan', 5, 250, 300, ['diesel', 'gasoline']],
+            ['Dacia', 'Sandero', 'hatchback', 5, 250, 300, ['diesel', 'gasoline']],
+            ['Dacia', 'Duster', 'suv', 5, 350, 450, ['diesel']],
+            ['Renault', 'Clio', 'hatchback', 5, 300, 350, ['diesel', 'gasoline']],
+            ['Peugeot', '208', 'hatchback', 5, 300, 380, ['diesel', 'gasoline', 'electric']],
+            ['Peugeot', '3008', 'suv', 5, 500, 650, ['diesel', 'hybrid']],
+            ['Volkswagen', 'Golf', 'hatchback', 5, 400, 500, ['diesel', 'gasoline']],
+            ['Toyota', 'Corolla', 'sedan', 5, 400, 500, ['hybrid']],
+            ['Hyundai', 'Tucson', 'suv', 5, 550, 700, ['diesel', 'hybrid']],
+            ['Kia', 'Picanto', 'hatchback', 4, 220, 270, ['gasoline']],
+            ['Mercedes', 'Classe C', 'sedan', 5, 900, 1200, ['diesel', 'hybrid']],
+            ['BMW', 'Série 4', 'coupe', 4, 1000, 1400, ['gasoline']],
+            ['Ford', 'Transit', 'van', 9, 600, 800, ['diesel']],
         ];
 
         $model = fake()->randomElement($models);
-        $energyType = fake()->randomElement(['gasoline', 'diesel', 'diesel', 'hybrid', 'electric']);
+        $energyType = fake()->randomElement($model[6]);
 
         // Energy rules (see ValidatesEnergyConsistency):
         // gasoline/diesel -> fuel only, electric -> range only, hybrid -> both
@@ -61,7 +61,8 @@ class CarFactory extends Factory
             'color' => fake()->randomElement(['Blanc', 'Noir', 'Gris', 'Bleu', 'Rouge']),
             'plate_number' => fake()->unique()->numerify('#####-A-##'),
             'transmission' => fake()->randomElement(['manual', 'automatic']),
-            'daily_price' => fake()->numberBetween(25, 120) * 10,
+            // Rounded to 10 MAD
+            'daily_price' => round(fake()->numberBetween($model[4], $model[5]), -1),
             'energy_type' => $energyType,
             'fuel_consumption' => $fuelConsumption,
             'electric_range' => $electricRange,
